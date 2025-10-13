2fc6415cdcf9b7a856c1934150f532d28791def9
humagana <hugomagana98@hotmail.com>
2025-10-12 23:09:53 -0600

    Update CourseController

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -57,12 +57,6 @@ public class CourseController {
         }
     }
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "UP";
-    }
-
-
     @GetMapping("/welcome")
     public String greetings() {
         return "Hello Techie , AWS CICD Example working fine !";
@@ -70,3 +64,4 @@ public class CourseController {
 
 
 }
+
